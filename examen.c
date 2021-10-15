5724c5e3fb9a7bd4997430d7d660797958db25a1
ErikVital <89081103+ErikVital@users.noreply.github.com>
2021-10-15 09:26:52 -0500

    Update examen.c

--- examen.c
+++ examen.c
@@ -3,49 +3,73 @@
 #define E 3
 #define M 4
 
-void captura(int clave[E], char nombre[50], float estaturaSueldo[E][M]);
+void captura(int clave[E], char nombre[50][E], float estaturaSueldo[E][M]);
+void imprime(int clave[E], char nombre[50][E], float estaturaSueldo[E][M], int empleado);
 void buscaValor(int arr[E][M], int *ma, int *posDos);
 int Bubuja(int A[E][M]);
 
 
 int main()
 {
+	int mayor=0;
+	int posicionMayor;
 	int empleado;
 	int clave[E];
-	char nombre[50]; //
+	char nombre[50][E]; //
 	float estaturaSueldo[E][M];
-	int mayor=0;
 	buscaValor(estaturaSueldo, &mayor, &posicionMayor);
 	empleado=Bubuja(estaturaSueldo);
 	
 	captura(clave, nombre, estaturaSueldo);
-	imprime(clave, nombre, estaturaSueldo;)
+	imprime(clave, nombre, estaturaSueldo, empleado);
 	
 	return 0;
 }
 
-void captura(int clave[E], char nombre[50], float estaturaSueldo[E][M])
+void captura(int clave[E], char nombre[50][E], float estaturaSueldo[E][M])
 {
 	int i;
 	int j=0;
 	
 	for(i=0; i<E; i++)
 	{
-		printf("Clave del alumno %d", i);
+		printf("Clave del alumno %d: ", i);
 		scanf("%d", &clave[i]);
 		
-		printf("Nombre del alumno %d", i);
-		scanf("%")
+		printf("Nombre del alumno %d: ", i);
+		scanf("%s", &nombre[i]);
+		
+		printf("Estatura y Sueldo del alumno %d: ", i);
+		scanf("%f", &estaturaSueldo[i][j]);
+		scanf("%f", &estaturaSueldo[i][j+1]);
 	}
 }
 
-
+void imprime(int clave[E], char nombre[50][E], float estaturaSueldo[E][M], int empleado)
+{
+	int i;
+	int j=0;
+	printf("Primero el de mayor sueldo\n\n");
+	printf("Clave  Nombre  Estatura  Sueldo\n");
+	printf("%d %s %.2f %.2f \n", clave[empleado], nombre[empleado], estaturaSueldo[empleado][j], estaturaSueldo[empleado][j+1]);
+	
+	for(i=0; i<E; i++)
+	{
+		printf("%d  ", clave[i]);
+		
+		printf("%s  ", nombre[i]);
+		
+		printf("%f  ", estaturaSueldo[i][j]);
+		printf("%f  ", estaturaSueldo[i][j+1]);
+		printf("\n");
+	}
+}
 
 void buscaValor(int arr[E][M], int *ma, int *posDos)
 {
 	int i;
 	
-	for(i=0; i<N; i++)
+	for(i=0; i<E; i++)
 	{
 		if(*ma<arr[0][i])
 		{
@@ -61,17 +85,18 @@ int Bubuja(int A[E][M])
 
     //for(p=1;p<N;p++)
    // {
-        for(cont=0;cont<N;cont++)
+        for(cont=0;cont<E;cont++)
         {
-            if(A[1][cont]>A[1][cont+1])
+            if(A[1][cont]<A[1][cont+1])
             {
                 aux=A[1][cont];
                 A[1][cont]=A[1][cont+1];
                 A[1][cont+1]=aux;
-                cont=p;
+                pos=cont;
             }
         }
    // }
    
-   return p;
+   return pos;
 }
+
